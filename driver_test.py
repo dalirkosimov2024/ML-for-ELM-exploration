17e8f144650437f780d8d3005703639a0ea20a56
D Kosimov <lcv510@kink.its.york.ac.uk>
2024-04-19 15:27:35 +0100

    Updated, now I can select the mode number

--- driver_test.py
+++ driver_test.py
@@ -1,10 +1,22 @@
 import os
+import numpy as np
 
 base_path = "/home/userfs/l/lcv510/pedestal/Results/circb_results"
 os.chdir(base_path)
 print(os.getcwd())
 
-for files in os.listdir(base_path):
-	if files.endswith(".in"):
-		print(files)
+line_array = np.array([])
+for filename in os.listdir(base_path):
+	if filename.endswith(".in"):
+		print(filename)
+
+		g = open(filename, "r")
+		for line in g:
+			line = line.strip()
+			columns = np.array(line.split())
+			print(columns)
+			line_array = np.append(line_array, columns)
+
+		g.close()
+		print(line_array[13])
 
